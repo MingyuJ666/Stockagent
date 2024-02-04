b45c3974feb2229a63effcba27ed718e9bbb4e76
Liuxinyi <liuxy0406@163.com>
2024-02-04 12:56:29 +0800

    bengkuilxy

--- agent.py
+++ agent.py
@@ -4,6 +4,18 @@ import random
 from prompt.agent_prompt import *
 from procoder.functional import format_prompt
 from procoder.prompt import *
+from utils import *
+
+def random_init(stock_initial_price):
+    stock, cash, debt = 0.0, 0.0, 0.0
+    while stock * stock_initial_price + cash < MIN_INITIAL_PROPERTY \
+            or stock * stock_initial_price + cash > MAX_INITIAL_PROPERTY\
+            or debt > stock * stock_initial_price + cash:
+        stock = random.uniform(0, MAX_INITIAL_PROPERTY / stock_initial_price)
+        cash = random.uniform(0, MAX_INITIAL_PROPERTY)
+        debt = random.uniform(0, MAX_INITIAL_PROPERTY)
+    return stock, cash, debt
+
 
 
 def run_api(model, prompt, temperature: float = 0):
@@ -20,24 +32,41 @@ def run_api(model, prompt, temperature: float = 0):
     return resp
 
 class Agent:
-    def __init__(self, i, secretary, model):
+    def __init__(self, i, stock_a_price, secretary, model):
         self.order = i
         self.secretary = secretary
         self.model = model
-        self.character = random.choice(["性格一", "性格二"])
-        self.stock_a = ?
+        self.character = random.choice(["性格一", "性格二"])  # todo
+        self.stock_a, self.cash, self.debt = random_init(stock_a_price)
         self.stock_b = 0
-        self.cash = ?
+        self.history = [[] for _ in range(TOTAL_DATE)]
+        self.loans = []
 
     def plan_loan(self, date):
-        prompt = Collection(LOAN_TYPE_PROMPT, DECIDE_IF_MOAN_PROMPT)
-        inputs = {
-            "date": date,
-            "character": self.character,
-            "stock_a": self.stock_a,
-            "stock_b": self.stock_b,
-            "cash": self.cash,
-        }
+        # first day action
+        if date == 1:
+            prompt = Collection(BACKGROUND_PROMPT, LOAN_TYPE_PROMPT, DECIDE_IF_MOAN_PROMPT)
+            inputs = {
+                "date": date,
+                "character": self.character,
+                "stock_a": self.stock_a,
+                "stock_b": self.stock_b,
+                "cash": self.cash,
+            }
+        # other day action
+        else:
+            prompt = Collection(LOAN_TYPE_PROMPT, DECIDE_IF_MOAN_PROMPT)
+            inputs = {
+                "date": date,
+                "character": self.character,
+                "stock_a": self.stock_a,
+                "stock_b": self.stock_b,
+                "cash": self.cash,
+            }
         resp = run_api(self.model, format_prompt(prompt, inputs), temperature=0)
+        loan = self.secretary.check_moan(date, resp)
+        self.loans.append(loan)
+        self.history[date].append(loan)
+
 
 
--- prompt/agent_prompt.py
+++ prompt/agent_prompt.py
@@ -1,5 +1,14 @@
-from procoder.functional import format_prompt, replaced_submodule
 from procoder.prompt import *
+from utils import *
+
+BACKGROUND_PROMPT = NamedBlock(
+    name="Background",
+    content="""
+        背景设定
+        任务描述
+        COT？
+    """
+)
 
 LOAN_TYPE_PROMPT = NamedVariable(
     refname="loan_type_prompt",
@@ -12,14 +21,17 @@ LOAN_TYPE_PROMPT = NamedVariable(
 )
 
 DECIDE_IF_MOAN_PROMPT = NamedBlock(
-    "Instruction",
-    """
-        现在是第{date}天，你当前的性格是{character}，持有{stock_a}股A公司股票，持有{stock_b}股B公司股票，现在你有{cash}元钱，你需要决定是否贷款和贷款金额。
-        贷款种类选择为{loan_type_prompt}
+    name="Instruction",
+    content="""
+        现在是第{date}天，你当前的性格是{character}，持有{stock_a}股A公司股票，持有{stock_b}股B公司股票，
+        现在你有{cash}元现金，{debt}元负债
+        你需要决定是否贷款和贷款金额。
+        贷款种类选择为{loan_type_prompt}，还款日在[65, 130, 195, 260]中决定。
         用json形式返回结果，例如：
-        {{{{"loan": "yes", "loan_type": "3", "closing_date": 30, "amount": 1000}}}}
+        {{{{"loan": "yes", "loan_type": "3", "closing_date": 130, "amount": 1000}}}}
         如果不需贷款，则返回：
         {{{{"loan" : "no"}}}}
     }
     """
 )
+
